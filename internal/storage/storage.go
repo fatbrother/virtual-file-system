7922e44079b83ed0aab70a0cec6cc2f959efe4c8
fatbrother <kevin92422@gmail.com>
2024-09-21 13:15:17 +0800

    refactor: use Trie to store data

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,18 +6,19 @@ import (
 	"sync"
 
 	"github.com/fatbrother/virtual-file-system/internal/user"
+	"github.com/fatbrother/virtual-file-system/pkg/trie"
 )
 
 // Storage represents the in-memory storage for the virtual file system
 type Storage struct {
-	users map[string]*user.User
+	users *trie.Trie
 	mu    sync.RWMutex
 }
 
 // NewStorage creates a new Storage instance
 func NewStorage() *Storage {
 	return &Storage{
-		users: make(map[string]*user.User),
+		users: trie.NewTrie(),
 	}
 }
 
@@ -27,7 +28,7 @@ func (s *Storage) AddUser(username string) error {
 	defer s.mu.Unlock()
 
 	lowercaseUsername := strings.ToLower(username)
-	if _, exists := s.users[lowercaseUsername]; exists {
+	if _, exists := s.users.Search(lowercaseUsername); exists {
 		return errors.New("user already exists")
 	}
 
@@ -36,7 +37,7 @@ func (s *Storage) AddUser(username string) error {
 		return err
 	}
 
-	s.users[lowercaseUsername] = newUser
+	s.users.Insert(lowercaseUsername, newUser)
 	return nil
 }
 
@@ -46,10 +47,37 @@ func (s *Storage) GetUser(username string) (*user.User, error) {
 	defer s.mu.RUnlock()
 
 	lowercaseUsername := strings.ToLower(username)
-	user, exists := s.users[lowercaseUsername]
-	if !exists {
-		return nil, errors.New("user not found")
+	if value, exists := s.users.Search(lowercaseUsername); exists {
+		if user, ok := value.(*user.User); ok {
+			return user, nil
+		}
+		return nil, errors.New("invalid user data")
 	}
+	return nil, errors.New("user not found")
+}
 
-	return user, nil
+
+// DeleteUser removes a user from the storage
+func (s *Storage) DeleteUser(username string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	lowercaseUsername := strings.ToLower(username)
+	if deleted := s.users.Delete(lowercaseUsername); !deleted {
+		return errors.New("user not found")
+	}
+	return nil
+}
+
+// ListUsers returns a list of all usernames with the given prefix
+func (s *Storage) ListUsers(prefix string) []string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	results := s.users.PrefixSearch(strings.ToLower(prefix))
+	usernames := make([]string, 0, len(results))
+	for username := range results {
+		usernames = append(usernames, username)
+	}
+	return usernames
 }
